3213dad6529c7765e5442ac6308104455ffdcd3e
Josh White <ciph3rzer0@gmail.com>
2026-01-24 12:32:07 -0500

    character movement

--- scenes/game_objects/box/box.gd
+++ scenes/game_objects/box/box.gd
@@ -1,4 +1,5 @@
 extends CharacterBody3D
+class_name MovableObject
 
 var current_position: Vector3i = Vector3i.ZERO
 var target_position: Vector3i = Vector3i.ZERO
@@ -7,24 +8,23 @@ var lerp_progress: float = 1.0
 # A box needs to hold discrete positions and move towards an adjacent tile
 
 func _get_discrete_position() -> Vector3i:
-		var discrete_x = round(global_transform.origin.x)
-		var discrete_y = round(global_transform.origin.y)
-		var discrete_z = round(global_transform.origin.z)
-		return Vector3i(discrete_x, discrete_y, discrete_z)
+    var discrete_x = round(global_transform.origin.x)
+    var discrete_y = round(global_transform.origin.y)
+    var discrete_z = round(global_transform.origin.z)
+    return Vector3i(discrete_x, discrete_y, discrete_z)
 
 
-func move() -> void:
-		current_position = _get_discrete_position()
-		target_position = current_position+ Vector3i(1, 0, 0)  # Example: move right by 1 unit
-		lerp_progress = 0.0
+func move(direction: Vector3i) -> void:
+    current_position = _get_discrete_position()
+    target_position = current_position + direction
+    lerp_progress = 0.0
 
 
 func _process(delta: float) -> void:
-		if Input.is_key_pressed(KEY_M):
-				move()
+    if Input.is_key_pressed(KEY_M):
+        move(Vector3i(1, 0, 0))
 
-		if lerp_progress < 1.0:
-				lerp_progress += delta
+    if lerp_progress < 1.0:
+        lerp_progress += delta
 
-		global_position = global_position.lerp(Vector3(target_position), lerp_progress)
-		pass
+    global_position = global_position.lerp(Vector3(target_position), lerp_progress)
--- scenes/game_objects/player/player.gd
+++ scenes/game_objects/player/player.gd
@@ -1,1 +1,16 @@
-extends CharacterBody3D
+extends MovableObject
+
+func _process(delta: float) -> void:
+    if Input.is_action_just_pressed("move_up"):
+        move(Vector3i(0, 0, -1))
+    if Input.is_action_just_pressed("move_left"):
+        move(Vector3i(-1, 0, 0))
+    if Input.is_action_just_pressed("move_down"):
+        move(Vector3i(0, 0, 1))
+    if Input.is_action_just_pressed("move_right"):
+        move(Vector3i(1, 0, 0))
+
+    if lerp_progress < 1.0:
+        lerp_progress += delta
+
+    global_position = global_position.lerp(Vector3(target_position), lerp_progress)
