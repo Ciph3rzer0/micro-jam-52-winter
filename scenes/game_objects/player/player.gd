a661efd0a8b3241a9f097760c9ef8810bd4eba38
Josh White <ciph3rzer0@gmail.com>
2026-01-24 14:32:25 -0500

    Pushing boxes around

--- scenes/game_objects/player/player.gd
+++ scenes/game_objects/player/player.gd
@@ -7,12 +7,12 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	if not discrete_position.is_moving:
 		if Input.is_action_pressed("move_up"):
-			LevelGrid.try_move_object(self, Vector3i(0, 0, -1))
+			LevelGrid.try_move_player(self, Vector3i(0, 0, -1))
 		elif Input.is_action_pressed("move_left"):
-			LevelGrid.try_move_object(self, Vector3i(-1, 0, 0))
+			LevelGrid.try_move_player(self, Vector3i(-1, 0, 0))
 		elif Input.is_action_pressed("move_down"):
-			LevelGrid.try_move_object(self, Vector3i(0, 0, 1))
+			LevelGrid.try_move_player(self, Vector3i(0, 0, 1))
 		elif Input.is_action_pressed("move_right"):
-			LevelGrid.try_move_object(self, Vector3i(1, 0, 0))
+			LevelGrid.try_move_player(self, Vector3i(1, 0, 0))
 
 	discrete_position.tick(delta)
--- scenes/game_scene/level_grid.gd
+++ scenes/game_scene/level_grid.gd
@@ -18,15 +18,33 @@ func add_object_to_grid(new_object: Node3D) -> void:
 	cells[position] = new_object.discrete_position
 	objects[new_object] = new_object.discrete_position
 
+# A player can push boxes.  So we move the box ahead one cell and the player takes up two cells temporarily.
+func try_move_player(moving_object: Node3D, relative_motion: Vector3i) -> bool:
+	assert_grid_object_is_valid(moving_object)
+	assert(relative_motion.length() == 1, "Relative motion must be to an adjacent cell.")
 
-func try_move_object(moving_object: Node3D, relative_motion: Vector3i) -> bool:
-	assert(objects.has(moving_object), "Node is not registered in the level grid.")
-	assert("discrete_position" in moving_object, "Node does not have a DiscretePosition component.")
-	assert(moving_object.discrete_position is DiscretePosition, "Node's discrete_position is not of type DiscretePosition.")
+	var new_position: Vector3i = moving_object.discrete_position.current_position + relative_motion
+
+	# Check if there is no object at target position or that it is push-able
+	var object_at_new_position: DiscretePosition = cells.get(new_position)
+
+	if(object_at_new_position != null):
+		# Try to push the object
+		var push_successful: bool = try_move_object(object_at_new_position.owner, relative_motion, true)
+		if(not push_successful):
+			print("Cannot move player because object cannot be pushed.")
+			return false
 	
-	if relative_motion.length() != 1:
-		printerr("Relative motion must be to an adjacent cell.")
-		assert(false)
+	if(cells.has(new_position)):
+		print("Cell is already occupied.")
+		return false
+
+	return try_move_object(moving_object, relative_motion)
+
+
+func try_move_object(moving_object: Node3D, relative_motion: Vector3i, move_instantly = false) -> bool:
+	assert_grid_object_is_valid(moving_object)
+	assert(relative_motion.length() == 1, "Relative motion must be to an adjacent cell.")
 
 	var new_position: Vector3i = moving_object.discrete_position.current_position + relative_motion
 	if(cells.has(new_position)):
@@ -36,12 +54,20 @@ func try_move_object(moving_object: Node3D, relative_motion: Vector3i) -> bool:
 	# Cell temporarily occupies two positions while moving	
 	cells[new_position] = moving_object.discrete_position
 
-	moving_object.discrete_position.move_stopped.connect(
-		func(previous_position: Vector3i, _current_position: Vector3i) -> void:
-		cells.erase(previous_position)
-	, CONNECT_ONE_SHOT
-	)
+	if move_instantly:
+		cells.erase(moving_object.discrete_position.current_position)
+	else:
+		moving_object.discrete_position.move_stopped.connect(
+			func(previous_position: Vector3i, _current_position: Vector3i) -> void:
+			cells.erase(previous_position)
+		, CONNECT_ONE_SHOT
+		)
 
 	moving_object.discrete_position.move(relative_motion)
 	
 	return true
+
+func assert_grid_object_is_valid(test_object: Node3D) -> void:
+	assert(objects.has(test_object), "Node is not registered in the level grid.")
+	assert("discrete_position" in test_object, "Node does not have a DiscretePosition component.")
+	assert(test_object.discrete_position is DiscretePosition, "Node's discrete_position is not of type DiscretePosition.")
