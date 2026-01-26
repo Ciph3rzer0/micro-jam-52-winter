8f97c6885acda54cefb59e2b96394ec51942e889
frkatona <apk5101@psu.edu>
2026-01-26 00:50:48 -0500

    added indicator (audio + vis)

--- scenes/game_objects/box/box.gd
+++ scenes/game_objects/box/box.gd
@@ -6,6 +6,9 @@ class_name MovableObject
 var discrete_position: DiscretePosition
 
 const AUDIO_SLIDE = preload("res://assets/audio/sfx/slide_1.mp3")
+const AUDIO_EXCHANGE = preload("res://assets/audio/sfx/jingle-uh-oh.mp3")
+
+var has_fallen: bool = false
 
 @onready var audio_player: AudioStreamPlayer3D = $AudioStreamPlayer3D
 
@@ -25,6 +28,29 @@ func _process(delta: float) -> void:
 				LevelGrid.try_move_object(self, Vector3i.DOWN)
 
 	discrete_position.tick(delta)
+	
+	if not has_fallen and global_position.y < -0.3:
+		_trigger_fall_effects()
+
+func _trigger_fall_effects() -> void:
+	has_fallen = true
+	audio_player.stream = AUDIO_EXCHANGE
+	audio_player.play()
+	
+	var label = Label3D.new()
+	label.text = "!"
+	label.font_size = 96
+	label.modulate = Color(1, 0.9, 0.4) # Gold/Yellow
+	label.billboard = BaseMaterial3D.BILLBOARD_ENABLED
+	# Add to main scene root so it doesn't move with the falling box/parent if parent is special
+	# But get_tree().current_scene might be main menu if testing, usually get_parent() is safe if it's the level
+	get_parent().add_child(label)
+	label.global_position = global_position + Vector3(0, 0.5, 0)
+	
+	var tween = create_tween()
+	tween.tween_property(label, "global_position", label.global_position + Vector3(0, 1.5, 0), 1.0).set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_CUBIC)
+	tween.parallel().tween_property(label, "modulate:a", 0.0, 1.0)
+	tween.tween_callback(label.queue_free)
 
 
 func set_convey_speed(conveyer: DiscretePosition) -> void:
